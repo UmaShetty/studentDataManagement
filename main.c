903c46d78767421ab9713189b9f7a4bc2976ee52
UmaShetty <umashettyc@gmail.com>
2024-03-25 15:43:15 +0530

    version1.0

--- main.c
+++ main.c
@@ -13,39 +13,54 @@ int main()
     printf("STUDENT DATABASE MANAGEMENT\n");
     printf("---------------------------\n");
 
-    printf("1.Student Records\n");
-    printf("2.Search for a student\n");
-    printf("3.Add a Student\n");
-    printf("4.Delete a Record\n");
-    printf("5.Update Student Details\n");
-    printf("6.Exit\n");
+    do{
 
-    printf("Enter your choice :\n");
-    scanf("%u",&choice);
+        printf("1.Student Records\n");
+        printf("2.Search for a student\n");
+        printf("3.Add a Student\n");
+        printf("4.Delete a Record\n");
+        printf("5.Update Student Details\n");
+        printf("6.Exit\n");
 
-    switch(choice)
-    {
-        case 1:studentRecords();
-        break;
-        case 2://search();
-        break;
-        case 3://add();
-        break;
-        case 4://deleteRecord();
-        break;
-        case 5://update();
-        break;
-        default://exit();
-        break;
-    }
+        printf("Enter your choice :\n");
+        scanf("%u",&choice);
+
+        switch(choice)
+        {
+            case 1:studentRecords();
+            break;
+            case 2://search();
+            break;
+            case 3://add();
+            break;
+            case 4://deleteRecord();
+            break;
+            case 5://update();
+            break;
+            case 6://exit();
+            break;
+            default:printf("Invalid Choice.Please enter a valid option.\n");
+        }
+    }while(choice!=6);
 }
 
-void studentRecords(name)
+void studentRecords()
 {
     //char name[50];
     printf("\n\n");
     puts("STUDENT RECORDS\n");
-    puts("---------------\n");
-    puts("Enter name of the student:\n");
-    fgets(name,50,stdin);
+    puts("---------------------------\n");
+
+    puts("1.Name of the student: Anu\n");
+    puts("  Roll No. :01\n");
+    puts("---------------------------\n");
+
+    puts("2.Name of the student: Asha\n");
+    puts("  Roll No. :02\n");
+    puts("---------------------------\n");
+
+    puts("3.Name of the student: Ram\n");
+    puts("  Roll No. :03\n");
+    puts("---------------------------\n");
+
 }
